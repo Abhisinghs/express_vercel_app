55022f1dd25e43611a6814fd651f059524b8b01b
Abhisinghs <abhisheksingh35716@gmail.com>
2023-11-13 13:25:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    console.log(`Express vercel app running`);
+})
+
+app.listen(4000,(req,res)=>{
+    console.log(`server is running at 4000`);
+})
